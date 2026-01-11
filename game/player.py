579844e1daf358a3d1a7b8f874067c0ffdaba1cb
admin <admin@archbox.com>
2026-01-11 01:04:33 -0800

    Implemented `Player` abstrace base class.
    The goal is to implement all different player strategies as unique
    players. I used an abstrace base class to enforce that all `Player`
    subclasses implement the `strategy` method before we get too far along
    our simulation (using `NotImplementedException` would only catch this
    issue during runtime).

---
+++ game/player.py
@@ -0,0 +1,84 @@
+"""
+game/player.py
+
+This file defines the base `Player` class. Each player manages their
+own state (the cards they carry) and their play strategy.
+"""
+
+from abc import ABC, abstractmethod
+from collections.abc import Collection, Iterable
+
+from .card import Card
+from .table import Table
+
+
+TakenCards = Collection[Collection[Card]]
+
+
+class IllegalStrategyException(Exception): ...
+
+
+class Player(ABC):
+    def __init__(self) -> None:
+        self.reset()
+
+    def play(self, table: Table) -> None:
+        play, take = self.strategy(table)
+
+        if play not in self._hand:
+            raise IllegalStrategyException(f"Cannot play {play}: not in hand.")
+        if not isinstance(take, Collection):
+            raise IllegalStrategyException(f"Cannot take {type(take)}.")
+        if not all(isinstance(sub, Collection) for sub in take):
+            taketype = ",".join(str(type(sub)) for sub in take)
+            raise IllegalStrategyException(f"Cannot take [{taketype}].")
+
+        self._hand.remove(play)
+
+        if len(take) == 0 or all(len(cards) == 0 for cards in take):
+            table.place(play)
+        else:
+            collected, cleared = table.take(play, take)
+            self._clears += int(cleared)
+            self._collection.extend(collected)
+
+    def receive_cards(self, cards: Iterable[Card]) -> None:
+        self._hand.extend(cards)
+
+    def reset(self) -> None:
+        """Reset the player to their original state."""
+        self._clears = 0        # Number of times this player cleared a table.
+        self._collection = []   # The cards collected during play.
+        self._hand = []         # The cards currently in the player's hand.
+
+    @property
+    def score(self) -> int:
+        return self._clears + sum(card.score for card in self._collection)
+
+    @abstractmethod
+    def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+        """
+        Each player must define a strategy which determines which
+        `Card` to play based on the player's current hand, the current
+        `Table` state, and possibly the total game history. The player
+        MUST ALWAYS play a single card. Players should ensure their
+        move is legal, though the table will enforce this and stop a
+        game for any illegal move.
+
+        The strategy should determine which cards the player wants to
+        take during their turn - a collection of a collection of cards.
+        Each sub-collection contains unique cards on the `table` that
+        constitue a legal move. A single `Card` can take multiple sub-
+        collections of cards: for example, an ACE can take the sub-
+        collections [A♠] and [2♥,9♣]. If legal, all of the cards
+        can be collected. If the collection of sub-collections is
+        empty, then the player will simply place their card on the
+        table and not collect anythying.
+
+        :param table: The table showing which cards can be taken and
+        which cards have been played throughout the game.
+        :returns: Returns a tuple with two items. The first is the card
+        played by a player, the second is the collection of collection
+        of cards the player wants to take.
+        """
+        ...
---
+++ tests/test_game_player.py
@@ -0,0 +1,203 @@
+import pytest
+
+from game.card import Card
+from game.player import (
+    IllegalStrategyException,
+    Player,
+    TakenCards,
+)
+from game.table import Table
+
+
+CARD_POINTS_SUM = 22
+
+
+@pytest.fixture
+def table() -> Table:
+    t = Table()
+    t.last = Card(0)    # Last card is A♠
+
+    # Place cards 1-4 (2♠, 3♠, 4♠, 5♠) onto the table.
+    for i in range(4):
+        t.place(Card(i + 1))
+
+    return t
+
+
+class MinimalPlayer(Player):
+    def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+        return Card(0), []
+
+
+class TestPlayerInitialize(object):
+    def test_unimplemented(self) -> None:
+        class IncompletePlayer(Player): ...
+
+        with pytest.raises(TypeError):
+            Player()
+        with pytest.raises(TypeError):
+            IncompletePlayer()
+
+    def test_implemented(self) -> None:
+        player = MinimalPlayer()
+
+        assert player._clears == 0
+        assert player._collection == []
+        assert player._hand == []
+
+
+class TestPlayerReceiveCards(object):
+    @pytest.mark.parametrize("ncards", [0, 1, 3])
+    def test_receive_empty_hand(self, ncards: int, table) -> None:
+        cards = [Card(i) for i in range(ncards)]
+        player = MinimalPlayer()
+        player.receive_cards(cards)
+        assert player._hand == cards
+
+    @pytest.mark.parametrize("ncards", [0, 1, 3])
+    def test_receive_not_empty_hand(self, ncards: int, table) -> None:
+        player = MinimalPlayer()
+        player._hand = [Card(i) for i in range(ncards)]
+        player.receive_cards(Card(i) for i in range(ncards, 2 * ncards))
+        assert player._hand == [Card(i) for i in range(2 * ncards)]
+
+
+class TestPlayerReset(object):
+    def test_reset(self) -> None:
+        player = MinimalPlayer()
+        player._clears = 1
+        player._collection = [Card(0)]
+        player._hand = [Card(0)]
+        player.reset()
+
+        assert player._clears == 0
+        assert player._collection == []
+        assert player._hand == []
+
+
+class TestPlayerScore(object):
+    def test_no_cards_no_clears(self) -> None:
+        assert MinimalPlayer().score == 0
+
+    def test_cards_standard(self) -> None:
+        cards = [Card(i) for i in range(52)]
+        player = MinimalPlayer()
+
+        for card in cards:
+            if card.score == 0:
+                player._collection.append(card)
+
+        assert player.score == 0
+        player._clears = 4
+        assert player.score == 4
+
+    def test_cards_with_points(self) -> None:
+        cards = [Card(i) for i in range(52)]
+        player = MinimalPlayer()
+
+        for card in cards:
+            if card.score > 0:
+                player._collection.append(card)
+
+        assert player.score == CARD_POINTS_SUM
+        player._clears = 4
+        assert player.score == CARD_POINTS_SUM + 4
+
+
+class TestPlayerPlay(object):
+    def test_play_illegal_not_in_hand(self, table) -> None:
+        class IllegalStrategyPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return Card(0), []
+
+        with pytest.raises(IllegalStrategyException):
+            IllegalStrategyPlayer().play(table)
+
+    def test_play_illegal_not_collection(self, table) -> None:
+        class IllegalStrategyPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return Card(0), Card(1)
+
+        player = IllegalStrategyPlayer()
+        player.receive_cards([Card(0)])
+
+        with pytest.raises(IllegalStrategyException):
+            player.play(table)
+
+    def test_play_illegal_not_collection_collection(self, table) -> None:
+        class IllegalStrategyPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return Card(0), [Card(1)]
+
+        player = IllegalStrategyPlayer()
+        player.receive_cards([Card(0)])
+
+        with pytest.raises(IllegalStrategyException):
+            player.play(table)
+
+    def test_play_illegal_move(self, table) -> None:
+        class IllegalStrategyPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return Card(0), [[Card(1)]]
+
+        player = IllegalStrategyPlayer()
+        player.receive_cards([Card(0)])
+
+        with pytest.raises(Exception):
+            player.play(table)
+
+    def test_play_legal_place(self, table) -> None:
+        class PlaceOnlyPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return self._hand[0], []
+
+        played = Card(0)
+        player = PlaceOnlyPlayer()
+        player.receive_cards([played])
+        player.play(table)
+
+        assert player._clears == 0
+        assert player._collection == []
+        assert player._hand == []
+
+        assert played in table.cards
+        assert played in table.history
+
+    def test_play_legal_take(self, table) -> None:
+        played = Card(14)   # 2♥
+        taken = Card(1)     # 2♠
+
+        class Take2SpadePlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return played, [[taken]]
+
+        player = Take2SpadePlayer()
+        player.receive_cards([played])
+        player.play(table)
+
+        assert player._clears == 0
+        assert player._hand == []
+        assert played in player._collection
+        assert taken in player._collection
+
+        assert taken not in table.cards
+        assert played in table.history
+
+    def test_play_legal_take_all(self, table) -> None:
+        class TakeAllPlayer(Player):
+            def strategy(self, table: Table) -> tuple[Card, TakenCards]:
+                return Card(6), [[Card(1), Card(4)], [Card(2), Card(3)]]
+
+        played = Card(6)    # 7♠ to take everything.
+        player = TakeAllPlayer()
+        player.receive_cards([played])
+        player.play(table)
+
+        assert player._clears == 1
+        assert player._hand == []
+
+        for i in [1, 2, 3, 4, 6]:
+            assert Card(i) in player._collection
+
+        assert table.cards == []
+        assert played in table.history
