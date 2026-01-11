2ab38aa390d72fbe88d7aa1367f47d32d70ad4f2
admin <admin@archbox.com>
2026-01-10 22:09:12 -0800

    Implemented `Table` class.
    The `Table` is how users interact with cards and ensure game state and
    any moved made are valid.

---
+++ game/table.py
@@ -0,0 +1,136 @@
+"""
+game/table.py
+
+This file defines the `Table` class. This class defines the common play
+area for the game and enforces the rules of Tablić.
+"""
+
+import itertools
+import math
+
+from typing import Collection, Iterable
+
+from .card import Card
+
+
+TakenCards = Collection[Collection[Card]]
+
+
+class IllegalMoveException(ValueError): ...
+
+
+class IllegalStateException(ValueError): ...
+
+
+class Table(object):
+    def __init__(self) -> None:
+        self.reset()
+
+    def __str__(self) -> str:
+        return "".join(str(c) for c in self.cards)
+
+    def _cards_sum_to(self, cards: TakenCards, values: tuple[int, ...]) -> bool:
+        for card_values in itertools.product(*[card.value for card in cards]):
+            if sum(card_values) in values:
+                return True
+        return False
+
+    def _validate_game_state(self, card: Card) -> None:
+        if card in self.cards:
+            raise IllegalStateException(f"{card} already on table - {self}")
+        if card in self.history:
+            raise IllegalStateException(f"{card} already played")
+
+    def place(self, card: Card) -> None:
+        self._validate_game_state(card)
+        self.cards.append(card)
+        self.history.append(card)
+
+    def reset(self) -> None:
+        self.cards = []
+        self.history = []
+        self.last = None
+
+    def show_table(self) -> str:
+        n = math.ceil(math.sqrt(len(self.cards)))
+        header = "[EMPTY TABLE]" if n == 0 else "[TABLE]"
+
+        print(f"{header} -- LAST: {self.last or 'None'}")
+        for i, card in enumerate(self.cards):
+            print(f"{str(card):>6s}")
+            if i % n == n - 1:
+                print()
+
+    def take(self, played: Card, taken: TakenCards) -> tuple[Iterable[Card], bool]:
+        """
+        Play a given card (`played`) and collect the cards in `taken`.
+        The `taken` variable is a collection of a collection of cards.
+        Each sub-collection will be checked against `played` to make
+        sure their value(s) sum up to the value of `played`.
+
+        This method should be used by `Player` implementations to
+        interact with the table. The table ensures that the following
+        game rules are followed:
+
+            RULE 1: There must be at least one card in `taken`.
+
+            RULE 2: The `played` card cannot be in `taken`.
+
+            RULE 3: Each card in `taken` must be on the table.
+
+            RULE 4: Each card in `taken` must be unique.
+
+            RULE 5: The sum of each collection of cards in `taken` must
+                    equal the value of `played`.
+
+        :param played: The card played by a `Player`.
+        :param taken: The card(s) taken by a `Player`.
+        :returns: A tuple with two items. The first item is an
+        interable that contains all cards a user can add to their
+        collection. The second item is a boolean that is True if the
+        `Table` is cleared, and False otherwise.
+        :raises IllegalMoveException: The move causes an illegal game
+        state. This is likely the fault of the simulator.
+        :raises IllegalMoveException: The move is not allowed by the
+        game rules. This is likely the fault of the player.
+        """
+        self._validate_game_state(played)
+        flat = list(itertools.chain.from_iterable(taken))
+
+        # RULE 1
+        if len(flat) < 1:
+            raise IllegalMoveException("player cannot take own card")
+
+        # RULE 2
+        if played in flat:
+            raise IllegalMoveException(f"card {played} played and taken")
+
+        # RULE 3, 4
+        if len(flat) > len(self.cards):
+            raise IllegalMoveException(
+                "player cannot take more cards than are on the table."
+            )
+
+        for i, card in enumerate(flat):
+            # RULE 3
+            if card not in self.cards:
+                raise IllegalMoveException(f"{card} not on table - {self}")
+
+            # RULE 4
+            if card in flat[i + 1:]:
+                raise IllegalMoveException(f"took card {card} more than once")
+
+        # RULE 5
+        for cards in taken:
+            if not self._cards_sum_to(cards, played.value):
+                cards = "".join(str(c) for c in self.cards)
+                raise IllegalMoveException(f"cannot take {cards} with {played}")
+
+        # Make sure we track `played` in our game history and remove any taken
+        # cards from the table.
+        self.history.append(played)
+        for card in flat:
+            self.cards.remove(card)
+
+        cleared = bool(len(self.cards) == 0)
+        return [played, *flat], cleared
---
+++ tests/test_game_table.py
@@ -0,0 +1,197 @@
+import itertools
+import pytest
+
+from game.card import Card
+from game.table import (
+    IllegalMoveException,
+    IllegalStateException,
+    Table,
+)
+
+
+@pytest.fixture
+def table() -> Table:
+    t = Table()
+    t.last = Card(0)
+
+    # Place cards 1-4 (2, 3, 4, 5 of spades) onto the table.
+    for i in range(4):
+        t.place(Card(i + 1))
+
+    return t
+
+
+class TestTableInitialize(object):
+    def test_initialize(self, table) -> None:
+        table = Table()
+
+        assert table.cards == []
+        assert table.history == []
+        assert table.last is None
+
+
+class TestTablePlace(object):
+    def test_legal(self) -> None:
+        cards = [Card(i) for i in range(52)]
+        table = Table()
+
+        for card in cards:
+            table.place(card)
+
+            assert table.cards[-1] == card
+            assert table.history[-1] == card
+
+    def test_illegal_state_already_on_table(self):
+        table = Table()
+        table.place(Card(0))
+
+        with pytest.raises(IllegalStateException):
+            table.place(Card(0))
+
+    def test_illegal_state_already_played(self):
+        table = Table()
+        table.history.append(Card(0))
+
+        with pytest.raises(IllegalStateException):
+            table.place(Card(0))
+
+
+class TestTableReset(object):
+    def test_reset(self, table) -> None:
+        assert table.cards == [Card(i + 1) for i in range(4)]
+        assert table.history == table.cards
+        assert table.last == Card(0)
+
+        table.reset()
+
+        assert table.cards == []
+        assert table.history == []
+        assert table.last is None
+
+
+class TestTableTake(object):
+    def test_illegal_own_card(self, table) -> None:
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [])
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [[]])
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [[], []])
+
+    def test_illegal_played_and_taken(self, table) -> None:
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [[Card(0)]])
+
+    def test_illegal_not_on_table(self, table) -> None:
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [[Card(51)]])
+
+    def test_illegal_repeated_card(self, table) -> None:
+        flat = [[Card(1), Card(2), Card(1)]]
+        nest = [[card] for card in flat[0]]
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), flat)
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), nest)
+
+    def test_illegal_too_many_cards(self, table) -> None:
+        ncards = len(table.cards) + 1
+        cards_flat = [[Card(i+1) for i in range(ncards)]]
+        cards_nest = [[Card(i+1)] for i in range(ncards)]
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), cards_flat)
+
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), cards_nest)
+
+    def test_illegal_invalid_sum(self, table) -> None:
+        with pytest.raises(IllegalMoveException):
+            table.take(Card(0), [[Card(1)]])
+
+    def test_valid_single_same(self) -> None:
+        table = Table()
+
+        for i in range(52):
+            played, taken = Card(i), Card((i + 13) % 52)
+
+            table.place(taken)
+            cards, cleared = table.take(played, [[taken]])
+
+            assert cards == [played, taken]
+            assert cleared
+            assert len(table.cards) == 0
+            assert table.history[-1] == played
+            table.reset()
+
+    def test_valid_multiple_same(self) -> None:
+        table = Table()
+
+        for i in range(52):
+            played = Card(i)
+            taken = [Card((i + j*13) % 52) for j in range(1, 4)]
+
+            for card in taken:
+                table.place(card)
+
+            cards, cleared = table.take(played, [[c] for c in taken])
+            assert cards == [played, *taken]
+            assert cleared
+            assert len(table.cards) == 0
+            assert table.history[-1] == played
+            table.reset()
+
+    @pytest.mark.parametrize("played,taken", [(
+        # 5spade = 2spade + 3spade [simple sum]
+        Card(4),
+        ((Card(1), Card(2),),),
+    ), (
+        # 5spade = 2spade + 3spade = 2heart + 3heart [multiple sum]
+        Card(4),
+        (
+            (Card(1), Card(2),),
+            (Card(14), Card(15),),
+        ),
+    ), (
+        # 5spade = 2spade + 3spade = 2heart + 3heart = 5heart [sum, take]
+        Card(4),
+        (
+            (Card(1), Card(2),),
+            (Card(14), Card(15),),
+            (Card(17),),
+        ),
+    ), (
+        # Aspade = 10spade + Aheart [ace 1 and 11]
+        Card(0),
+        ((Card(9), Card(13)),),
+    ), (
+        # Aspade = Aheart = Adiamond = Aclub
+        Card(0),
+        ((Card(13),), (Card(26),), (Card(39),),),
+    )])
+    def test_valid_sums(self, played: Card, taken: tuple) -> None:
+        flat = list(itertools.chain.from_iterable(taken))
+
+        table = Table()
+        for card in flat:
+            table.place(card)
+
+        cards, cleared = table.take(played, taken)
+        assert cards == [played, *flat]
+        assert cleared
+        assert len(table.cards) == 0
+        assert table.history[-1] == played
+
+    def test_valid_cards_remain(self) -> None:
+        table = Table()
+        table.cards = [Card(i+1) for i in range(13)]
+
+        cards, cleared = table.take(Card(0), [[Card(13)]])
+        assert cards == [Card(0), Card(13)]
+        assert not cleared
+        assert table.cards == [Card(i+1) for i in range(12)]
+        assert table.history == [Card(0)]
